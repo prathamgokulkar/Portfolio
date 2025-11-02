028103d1b473fc16c3405da5b7af6b021803edcc
Prathamesh Gokulkar <prathamgokulkar@gmail.com>
2025-11-02 20:59:00 +0530

    link update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+//https://drive.google.com/file/d/1A4kg88HPDDD4rS6ruMJfi3aWpqFdSPHx/view?usp=drive_link
